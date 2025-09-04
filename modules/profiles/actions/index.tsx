fcc0a1c30cdde95b377e41a5364ca01352851559
Alok <alok.dash787@gmail.com>
2025-09-04 21:11:20 +0530

    commits 2

--- modules/profiles/actions/index.tsx
+++ modules/profiles/actions/index.tsx
@@ -1,2 +1,39 @@
 "use server"
+import { db } from "@/lib/db";
+import { getAvailableUsernameSuggestions } from "../utils";
+import { currentUser } from "@clerk/nextjs/server";
 
+
+export const checkProfileUsernameAvailability = async (username: string) => {
+  if (!username) return { available: false, suggestions: [] };
+
+  const user = await db.user.findUnique({ where: { username } });
+
+  if (!user) return { available: true };
+
+  const suggestions = await getAvailableUsernameSuggestions(username, 3, 10);
+ 
+  return { available: false, suggestions };
+};
+
+
+
+export const claimUsername = async (username: string) => {
+  const loggedInUser = await currentUser();
+
+  if (!loggedInUser) return { success: false, error: "No authenticated user found" };
+  
+  const user = await db.user.update({
+    where:{
+        clerkId: loggedInUser.id
+    },
+    data: {
+        username: username
+    }
+  });
+
+  if (!user) return { success: false, error: "No authenticated user found" };
+
+  return { success: true };
+
+}
